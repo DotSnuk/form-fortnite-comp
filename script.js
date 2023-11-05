16aa3c3d70b41c28613e8ffe00f500e69d4102bc
Hampus Farner <hampusfarner@outlook.com>
2023-11-05 13:50:31 +0100

    working on for loop to create li/ul

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const squadRB = document.querySelectorAll('.squadInput');
 squadRB.forEach(o => {
-        o.addEventListener('click', e => {
-            createInput();
+        o.addEventListener('click', () => {
+            createList(getSquadSize);
     })
 })
 
@@ -27,9 +27,17 @@ function createInput(){
     nameContainer.appendChild(input);
 }
 
-function createList(){
+function createList(n){
     const nameContainer = document.querySelector('.names');
     const list = document.createElement('ul');
+    nameContainer.appendChild(list);
+    // for loop with n
+    for (var i = 0; i < n; i++){
+        // needs to be resetted each time radiobutton is clicked #futurenote
+
+    }
     const lItem = document.createElement('li');
+    
+    list.appendChild(lItem);
 }
 /* create function that first makes <li>, then for each input, have them in a <ul> */

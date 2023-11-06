e1b70cee542f89f758e6772b02035b517ce04622
Hampus Farner <hampusfarner@outlook.com>
2023-11-06 13:38:33 +0100

    inputs text field with id for each player. working on resetting if the radio button is clicked more than once

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const squadRB = document.querySelectorAll('.squadInput');
 squadRB.forEach(o => {
         o.addEventListener('click', () => {
-            createList(getSquadSize);
+            if (document.getElementsByClassName('names').firstChild){
+                console.log('has children!');
+            }
+            createList(getSquadSize(o.id));
     })
 })
 
@@ -31,13 +34,25 @@ function createList(n){
     const nameContainer = document.querySelector('.names');
     const list = document.createElement('ul');
     nameContainer.appendChild(list);
-    // for loop with n
     for (var i = 0; i < n; i++){
         // needs to be resetted each time radiobutton is clicked #futurenote
-
+        const lItem = document.createElement('li');
+        list.appendChild(lItem);
+        let nameLabel = document.createElement('label');
+        nameLabel.innerText = `Player ${i+1}`;
+        nameLabel.setAttribute('for', `name${i+1}`);
+        lItem.appendChild(nameLabel);
+        let nameInput = document.createElement('input');
+        nameInput.type = 'text';
+        nameInput.id = `name${i+1}`;
+        nameInput.name = 'playerName';
+        lItem.appendChild(nameInput);
     }
-    const lItem = document.createElement('li');
-    
-    list.appendChild(lItem);
+}
+
+function resetNames(){
+    while(document.querySelector('.names').firstChild){
+        document.removeChild('.names');
+    }
 }
 /* create function that first makes <li>, then for each input, have them in a <ul> */
